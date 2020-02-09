877a3a0234821ab61ddd06ce9d3d5f7c75f16505
seungha0802 <seungha0802@gmail.com>
2020-02-09 21:09:22 +0900

    vote router 생성

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,22 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const vote = require('./vote');
+const key = require('./key.json')
 
-admin.initializeApp();
+admin.initializeApp({
+  credential: admin.credential.cert(key)
+});
 
 const db = admin.firestore();
 
+exports.vote = functions.https.onRequest(vote(db));
+
 exports.createUser = functions.auth.user().onCreate(async (user) => {
-  const claims = { level: 2 };
-  await admin.auth().setCustomUserClaims(user.uid, claims);
-  admin.auth().setCustomUserClaims(user.uid, { level: 0 })
+  const { uid, email, emailVerified, disabled } = user;
+  return await db.collection('users').doc(uid).set({
+    uid,
+    email,
+    emailVerified,
+    disabled
+  });
 });
